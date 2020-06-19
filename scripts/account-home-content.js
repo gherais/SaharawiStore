295f89817f28ee7240624949d1c9133fba3f9fd0
gherais <benaissa.m.gherais@gmail.com>
2020-06-19 16:13:38 -0400

    account sidemenu toggle

--- scripts/account-home-content.js
+++ scripts/account-home-content.js
@@ -3,10 +3,12 @@ var articlesHeadersListElements = document.querySelectorAll(".link-header");
 
 function articleMouseEntered(target) {
     target.classList.add("active");
-    console.log(target.parentNode.classList)
+    target.querySelector("i").classList.add("active");
+    target.querySelector("h3").classList.add("active");
 }
 
 function articleMouseLeft(target) {
     target.classList.remove("active");
-    console.log(target.parentNode.classList)
+    target.querySelector("i").classList.remove("active");
+    target.querySelector("h3").classList.remove("active");
 }
