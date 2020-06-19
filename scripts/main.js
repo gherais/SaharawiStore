50ea80f64c935c8556b7d325465ff72bb2f9612a
gherais <benaissa.m.gherais@gmail.com>
2020-06-18 21:43:51 -0400

    Project s first commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var links = document.getElementById("links");
+var categoriesList = document.getElementById("categories");
+
+function burgerClicked(event) {
+    links.classList.toggle("hide");
+}
+
+function mouseInLinks(event) {
+    categories.classList.add('active');
+}
+
+function mouseOutLinks(event) {
+    categories.classList.remove('active')
+}
